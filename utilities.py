f91c4b4def470e3837c1d893607cdcd8e95cd018
Ruder <mika.ruder@ua1d0qpyf91rf.mynetworksettings.com>
2024-09-09 21:32:29 -0400

    final changes

--- utilities.py
+++ utilities.py
@@ -5,12 +5,10 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 import plotly.graph_objects as go
 from prophet import Prophet
+from prophet.diagnostics import cross_validation, performance_metrics
+from prophet.plot import plot_cross_validation_metric
 import holidays
 from xgboost import XGBRegressor as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
-def calculate_mape(actual, predict):
-    absolute_percentage_error = np.abs((actual - predict) / actual)
-    mape = round(np.mean(absolute_percentage_error) * 100,2)
-    return mape
